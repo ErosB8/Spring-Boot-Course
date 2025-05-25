82ac5423559b654d8d80d78a8e8422b0d10c422e
ErosB8 <eros0088@hotmail.com>
2025-05-25 19:53:05 -0300

    7

---
+++ springbootcourse/src/main/java/com/example/demo/controllers/EjemploController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EjemploController {
+	
+	@GetMapping("/detalles_info")
+	
+	
+	public String info(Model model) {
+		
+		model.addAttribute("Titulo", "Servidor en línea");
+		model.addAttribute("Prueba", "SpringBoot");
+		model.addAttribute("Max", "El perro de Eros");
+
+		return "detalles_info"; 
+		
+	}
+	
+}
